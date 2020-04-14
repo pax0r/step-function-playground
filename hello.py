7bb1eb4853e5caf666ef75159e3594221305f500
Bartlomiej Biernacki <pax0r@o2.pl>
2020-04-14 12:02:38 +0200

    Fixed input and output as json strings and added error handling

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import json
 import time
 import uuid
 
@@ -9,23 +10,37 @@ WORKER_NAME = 'worker-{}'.format(uuid.uuid4())
 
 
 def work_function(input_data):
+    input_data = json.loads(input_data)
     who = input_data.get('who', 'Anonymous')
     time.sleep(2)
-    return f"Hello {who}!"
+    return json.dumps({'result': f"Hello {who}!"})
 
 
 if __name__ == '__main__':
     print(f"Starting worker {WORKER_NAME}")
+    client = boto3.client('stepfunctions')
     while True:
-        client = boto3.client('stepfunctions')
         response = client.get_activity_task(
             activityArn=ACTIVITY_ARN,
             workerName=WORKER_NAME,
         )
-        print(f"Got activity task with data: {response}")
-        result = work_function(response['input'])
-        client.send_task_success(
-            taskToken=response['token'],
-            output=result
-        )
-        print("Success sent to AWS")
+        token = response.get('taskToken')
+        if token:
+            print(f"Got activity task with data: {response}")
+            try:
+                result = work_function(response['input'])
+                client.send_task_success(
+                    taskToken=response['taskToken'],
+                    output=result
+                )
+                print("Success sent to AWS")
+            except Exception as exc:
+                print("Error on processing task: ", exc)
+                client.send_task_failure(
+                     taskToken=response['taskToken'],
+                     error=str(exc),
+                     cause=repr(exc),
+                )
+        else:
+            print("No task, wait a little bit before next fetch")
+            time.sleep(1)
